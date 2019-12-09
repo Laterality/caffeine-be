4818dd8fd048e2c8ba2a4001da9f4637c7354870
Jinwoo Shin <bluesky0255@gmail.com>
2019-12-09 21:20:21 +0900

    Reduce duplicated code for notification feature

--- src/main/java/com/woowacourse/caffeine/application/service/CustomerNotificationService.java
+++ src/main/java/com/woowacourse/caffeine/application/service/CustomerNotificationService.java
@@ -1,44 +1,18 @@
 package com.woowacourse.caffeine.application.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class CustomerNotificationService {
 
-    private static final Logger logger = LoggerFactory.getLogger(CustomerNotificationService.class);
-
-    private Map<String, ResponseBodyEmitter> subscriptions = new HashMap<>();
+    private NotificationService<String> notificationService = new NotificationService<>();
 
     public ResponseBodyEmitter subscribe(final String customerId) {
-        ResponseBodyEmitter emitter = new SseEmitter();
-        emitter.onCompletion(remove(customerId));
-        emitter.onTimeout(remove(customerId));
-        emitter.onError((e) -> {
-            logger.error("Error in event emitter", e);
-            subscriptions.remove(customerId);
-        });
-
-        subscriptions.put(customerId, emitter);
-        return emitter;
+        return notificationService.subscribe(customerId);
     }
 
-    private Runnable remove(final String id) {
-        return () -> subscriptions.remove(id);
-    }
-
-    public void send(final String shopId, final String message) {
-        try {
-            subscriptions.get(shopId).send(message);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void send(final String customerid, final String message) {
+        notificationService.send(customerid, message);
     }
 }
